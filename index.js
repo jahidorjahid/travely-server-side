5eff5c3c168f08fff549aa7f824d9b9a85a0d351
jahidorjahid <zahid.rdx71@gmail.com>
2021-11-08 04:47:35 +0600

    create first api

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const port = 5000;
 require("dotenv").config();
 
+// middleware
+app.use(express.json());
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q3lrf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
@@ -12,12 +15,14 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
+    await client.connect();
     const database = client.db("travely");
-    const roomCollection = database.collection("room");
+    const roomCollection = database.collection("rooms");
 
-    app.get("/", (req, res) => {
+    app.get("/rooms", async (req, res) => {
       console.log("mongodb connected sucess");
-      res.send("hello node js");
+      const result = await roomCollection.find({}).toArray();
+      res.json(result);
     });
   } finally {
     // client.close()
