38a2c4264b00932b3d97c4da66e2aaf7524f830d
jahidorjahid <zahid.rdx71@gmail.com>
2021-11-11 01:21:47 +0600

    create get single room api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { MongoClient } = require("mongodb");
+const { MongoClient, ObjectId } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
@@ -19,17 +19,28 @@ async function run() {
   try {
     await client.connect();
     const database = client.db("travely");
-    const roomCollection = database.collection("rooms");
+    const roomsCollection = database.collection("rooms");
 
     // test GET API
     app.get("/", (req, res) => {
       res.send("This is Test API");
     });
-
+    // GET all rooms
     app.get("/rooms", async (req, res) => {
       console.log("mongodb connected sucess");
-      const result = await roomCollection.find({}).toArray();
+      const result = await roomsCollection.find({}).toArray();
       res.json(result);
+    });
+
+    // GET room by id
+    app.get("/rooms/:id", async (req, res) => {
+      const roomId = req.params.id;
+      const query = { _id: ObjectId(roomId) };
+      const room = await roomsCollection.findOne(query);
+
+      if (room._id == roomId) {
+        res.send(room);
+      }
     });
   } finally {
     // client.close()
