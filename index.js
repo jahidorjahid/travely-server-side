9cb3695d27cebbede27913184c60849ed1f528eb
jahidorjahid <zahid.rdx71@gmail.com>
2021-11-11 00:34:05 +0600

    use cors

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require("express");
 const { MongoClient } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require("cors");
 require("dotenv").config();
 
 // middleware
 app.use(express.json());
+app.use(cors());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.q3lrf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
